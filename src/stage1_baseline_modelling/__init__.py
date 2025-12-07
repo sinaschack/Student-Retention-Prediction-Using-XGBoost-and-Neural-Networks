94f46da1c57b72731aaf70b52c0e336f150d8b7d
Sina Schack <168820373+sinaschack@users.noreply.github.com>
2025-12-07 18:48:01 +0000

    Update __init__.py

--- src/stage1_baseline_modelling/__init__.py
+++ src/stage1_baseline_modelling/__init__.py
@@ -1,1 +1,6 @@
-
+# src/stage1/__init__.py
+from .data_preprocessing import load_data, drop_unhelpful_columns, handle_missing_values, encode_target
+from .feature_engineering import create_age, one_hot_encode
+from .model_xgboost import train_default_xgb, xgb_hyperparameter_tuning
+from .model_nn import build_nn
+from .evaluation import evaluate_model
